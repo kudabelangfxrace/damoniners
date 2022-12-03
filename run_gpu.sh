aa7298c9ed1816bd3bf9e4367089df420f1c7691
VollarBelang <36155375+kudabelangfxrace@users.noreply.github.com>
2022-12-03 18:38:46 +0800

    asdsd
    asdsd

--- run_gpu.sh
+++ run_gpu.sh
@@ -3,8 +3,7 @@ if ps aux | grep 'damominer' | grep -q 'proxy'; then
     echo "DamoMiner already running."
     exit 1
 else
-    nohup ./damominer --address aleo15ketg9ffnlrf7u6s4kz9k498lpq04njl5za3kfl3wtr22k5gwcfqqvdgjq --proxy asia1.damominer.hk:9090 >> aleo.log 2>&1 &
-fi
+    nohup ./damominer --address aleo1p6xnyqrz34l8khlwv2z27qne0yfar9f2yt7nkjxywvmmspc67szs50x9el --proxy aleo1.damominer.hk:9090 --worker worker_xxx >> aleo.log 2>&1 &
 # 此模板仅供参考，使用请按个人实际参数进行修改
 # 钱包：“aleoxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx”修改为个人钱包地址
 # 代理地址：“asia1.damominer.hk:9090”修改为官方发布的代理地址
