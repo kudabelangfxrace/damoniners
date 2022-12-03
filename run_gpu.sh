09627d82ac3884689f32ab34a7f9ab53b8db741a
VollarBelang <36155375+kudabelangfxrace@users.noreply.github.com>
2022-12-03 18:27:37 +0800

    sdsd
    asd

---
+++ run_gpu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if ps aux | grep 'damominer' | grep -q 'proxy'; then
+    echo "DamoMiner already running."
+    exit 1
+else
+    nohup ./damominer --address aleoxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx --proxy asia1.damominer.hk:9090 >> aleo.log 2>&1 &
+fi
+# 此模板仅供参考，使用请按个人实际参数进行修改
+# 钱包：“aleoxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx”修改为个人钱包地址
+# 代理地址：“asia1.damominer.hk:9090”修改为官方发布的代理地址
+# 此模板默认不设置自定义矿工名，如有需求可根据教程进行配置
